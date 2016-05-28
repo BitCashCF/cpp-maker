c69e3a9d16204c062a0b3998c1c79938005c7a55
Alexander Schrab <meros@Alexanders-MBP.my.lan>
2016-05-28 11:21:39 +0200

    Compiles with gcc 4.9! Who needs 5.1?

--- maker/platforms/nrf51822/toolchain.cmake
+++ maker/platforms/nrf51822/toolchain.cmake
@@ -4,8 +4,8 @@ SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
+CMAKE_FORCE_C_COMPILER(/usr/local/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(/usr/local/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-g++ GNU)
 
 
 SET(TEMPLATE_PATH ${CMAKE_CURRENT_LIST_DIR}/SDK/components/toolchain/gcc)
@@ -13,7 +13,7 @@ SET(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/gcc_nrf51.ld)
 
 #flags common to all targets
 SET(CFLAGS "-DNRF51 -DBOARD_PCA10028 -DBSP_DEFINES_ONLY -mcpu=cortex-m0 -mthumb -mabi=aapcs -Wall -Werror -O3 -g3 -mfloat-abi=soft -ffunction-sections -fdata-sections -fno-strict-aliasing -fno-builtin --short-enums")
-SET(LDFLAGS "-Xlinker -Map=output.map -mthumb -mabi=aapcs -L ${TEMPLATE_PATH} -T${LINKER_SCRIPT} -mcpu=cortex-m0 -Wl,--gc-sections -lc") #TODO: --specs=nano.specs  -lnosys
+SET(LDFLAGS "-Xlinker -Map=output.map -mthumb -mabi=aapcs -L ${TEMPLATE_PATH} -T${LINKER_SCRIPT} -mcpu=cortex-m0 -Wl,--gc-sections --specs=nano.specs  -lnosys -lc")
 SET(ASMFLAGS "-x assembler-with-cpp -DNRF51 -DBOARD_PCA20006 -DBSP_DEFINES_ONLY")
 
 
